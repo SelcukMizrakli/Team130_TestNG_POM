6799f743e992851486a1a68ae7610391877b80be
Selcuk <mizrakli12385@mgil.com>
2024-01-25 21:58:27 +0300

    TestNG

---
+++ src/test/java/tests/day15_TestNG_Pom/C02_Priority.java
@@ -0,0 +1,27 @@
+package tests.day15_TestNG_Pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C02_Priority {
+    // https://www.testotomasyonu.com/ adresine gidin.
+    // Olusturacaginiz 3 farkli test method’unda asagida verilen görevleri yapin.
+    // 1. Test : Testotomasyonu ana sayfaya gittiginizi test edin
+    // 2. Test : search Box’i kullanarak “phone” icin arama yapin
+    //           ve arama sonucunda urun bulunabildigini test edin
+    // 3. Test : ilk urunu tiklayin ve urun isminin case sensitive olmaksizin phone icerdigini test edin
+
+    @Test
+    public void testOtomasyonAnaSayfaTesti(){
+
+        Driver.getDriver().get("https://www.testotomasyonu.com");
+        String expectedUrl = "https://www.testotomasyonu.com/";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+
+        Assert.assertEquals(actualUrl,expectedUrl);
+    }
+
+}
