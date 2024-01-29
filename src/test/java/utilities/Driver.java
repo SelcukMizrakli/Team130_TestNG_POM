cd67274a3e178e04c1bb7cb95aa2de228ea611ed
Selcuk <mizrakli12385@mgil.com>
2024-01-29 22:45:05 +0300

    day17

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,8 +1,10 @@
 package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.OperaDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import java.time.Duration;
 
@@ -16,9 +18,39 @@ public class Driver {
 
     public static WebDriver getDriver(){
 
-        WebDriverManager.chromedriver().setup();
+        String browserTercihi = ConfigReader.getProperty("browser");
+
+        /*
+            Browser'in sadece chrome olmamasi icin
+            configuration.properties'e browser = edge
+            secenegi ekledik.
+
+            Orada yazan browser tercihini 21. satirda alip
+            tercihe uygun driver olusturmasi icin
+            bir switch statement kullandik.
+        */
+
+
         if (driver == null){
-            driver = new ChromeDriver();
+
+            switch (browserTercihi){
+
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+
+            }
+
+
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
